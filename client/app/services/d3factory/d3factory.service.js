c70c1c036d58475bdc00a88997b5017875e651d1
Ryan Villanueva <ryanvill@gmail.com>
2015-02-05 14:24:57 -0500

    Force draw numbering fix

--- client/app/services/d3factory/d3factory.service.js
+++ client/app/services/d3factory/d3factory.service.js
@@ -4,19 +4,64 @@ angular.module('ariadneApp')
   .factory('d3Factory', function ($q) {
     // Service logic
 
-    var force = function(){
+    var drawForce = function(graphData){
+        console.log('drawing')
+        var width = 960,
+            height = 500
+
+        var svg = d3.select("#canvas").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        var force = d3.layout.force()
+            .gravity(.1)
+            .distance(75)
+            .charge(-100)
+            .size([width, height]);
+
+        force
+            .nodes(graphData.nodes)
+            .links(graphData.links)
+            .start();
+
+        var link = svg.selectAll(".link")
+            .data(graphData.links)
+          .enter().append("line")
+            .attr("class", "link");
+
+        var node = svg.selectAll(".node")
+            .data(graphData.nodes)
+          .enter().append("g")
+            .attr("class", "node")
+            .call(force.drag);
+
+        node.append("image")
+            .attr("xlink:href", "https://github.com/favicon.ico")
+            .attr("x", -8)
+            .attr("y", -8)
+            .attr("width", 16)
+            .attr("height", 16);
+
+        node.append("text")
+            .attr("dx", 12)
+            .attr("dy", ".35em")
+            .text(function(d) { return d.name });
+
+        force.on("tick", function() {
+          link.attr("x1", function(d) { return d.source.x; })
+              .attr("y1", function(d) { return d.source.y; })
+              .attr("x2", function(d) { return d.target.x; })
+              .attr("y2", function(d) { return d.target.y; });
+
+          node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+        });
 
     }
 
     // Public API here
     return {
-      force: function (data) {
-        var deferred = $q.defer();
-
-        var entities = data.rep.doc[0].entities[0].entity;
-        var links = data.rep.doc[0].relations[0].relation;
-
-        return graphData;
+      drawForce: function (data) {
+        drawForce(data)
       }
     };
   });
